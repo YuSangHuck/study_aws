81ebf1c96fa79b4822b8bec5e13f16c3f406cafc
YuSangHuck <g01099387636@gmail.com>
2020-09-23 14:24:59 +0900

    Update deploy script argument

--- lambda/go/deploy.ps1
+++ lambda/go/deploy.ps1
@@ -6,13 +6,14 @@
 ##
 # FIXME x aws configure 해결해야 함
 
-$ContainerName = $args[0]
-$LambdaFuncName = "hello-lambda-golang"
-$S3BucketName = "hello-lambda-golang"
+$defaultContainerName = "demo"
+$containerName = If ($args[0] -eq $null) { $defaultContainerName } Else { $args[0] }
+$lambdaFuncName = "hello-lambda-golang"
+$s3BucketName = "hello-lambda-golang"
 
-docker exec $ContainerName sh -c "go env"
+docker exec $containerName sh -c "go env"
 
-docker exec $ContainerName sh -c "env GOOS=linux go build -v -ldflags '-d -s -w' -a -tags netgo -installsuffix netgo -o main main.go &&
+docker exec $containerName sh -c "env GOOS=linux go build -v -ldflags '-d -s -w' -a -tags netgo -installsuffix netgo -o main main.go &&
 zip function.zip main &&
-aws lambda update-function-code --function-name $LambdaFuncName \
+aws lambda update-function-code --function-name $lambdaFuncName \
   --zip-file fileb://function.zip"
