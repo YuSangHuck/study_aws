255683b879f51dfc6faeef24ce4798209e409f30
YuSangHuck <g01099387636@gmail.com>
2020-09-28 16:59:04 +0900

    Update handler from main package

--- serverless/lambda/com/handler.go
+++ serverless/lambda/com/handler.go
@@ -9,15 +9,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func test(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"success": true, "message": "test lambda API"})
+func test1(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"success": true, "message": "test1 lambda API"})
+}
+func test2(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"success": true, "message": "test2 lambda API"})
 }
 
 func main() {
 	addr := ":3000"
 	mode := os.Getenv("GIN_MODE")
 	g := gin.New()
-	g.GET("/test", test)
+	g.GET("/test1", test1)
+	g.GET("/test2", test2)
 
 	if mode == "release" {
 		log.Fatal(gateway.ListenAndServe(addr, g))
