0fb893d7be3496772409bccfebc6588cbc0fd31b
YuSangHuck <g01099387636@gmail.com>
2020-09-29 18:36:52 +0900

    test

--- serverless/lambda/com/handler.go
+++ serverless/lambda/com/handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
@@ -16,12 +17,17 @@ func test2(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"success": true, "message": "test2 lambda API"})
 }
 
+func lambdaSDKHandler(ctx context.Context) (string, error) {
+	return "반갑수다!", nil
+}
+
 func main() {
 	addr := ":3000"
 	mode := os.Getenv("GIN_MODE")
 	g := gin.New()
 	g.GET("/test1", test1)
 	g.GET("/test2", test2)
+	g.GET("/lambda-sdk", lambdaSDKHandler)
 
 	if mode == "release" {
 		log.Fatal(gateway.ListenAndServe(addr, g))
