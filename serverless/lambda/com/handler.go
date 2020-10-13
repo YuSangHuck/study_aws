4336906000baa251f228843d30864c92d72f82e6
YuSangHuck <g01099387636@gmail.com>
2020-10-13 11:53:46 +0900

    Add lambdaSDKHandler

--- serverless/lambda/com/handler.go
+++ serverless/lambda/com/handler.go
@@ -1,37 +1,59 @@
 package main
 
 import (
+	//
+	// implement with aws-sdk-lambda
+	//
 	"context"
-	"log"
-	"net/http"
-	"os"
 
-	"github.com/apex/gateway"
-	"github.com/gin-gonic/gin"
-)
+	"github.com/aws/aws-lambda-go/lambda"
+	/*
+		//
+		// implement with gin-gonic
+		//
+		"log"
+		"net/http"
+		"os"
+		"github.com/apex/gateway"
+		"github.com/gin-gonic/gin"
+	*/)
 
+/*
+//
+// implement with gin-gonic
+//
 func test1(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"success": true, "message": "test1 lambda API"})
 }
 func test2(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"success": true, "message": "test2 lambda API"})
 }
+*/
 
 func lambdaSDKHandler(ctx context.Context) (string, error) {
 	return "반갑수다!", nil
 }
 
 func main() {
-	addr := ":3000"
-	mode := os.Getenv("GIN_MODE")
-	g := gin.New()
-	g.GET("/test1", test1)
-	g.GET("/test2", test2)
-	g.GET("/lambda-sdk", lambdaSDKHandler)
+	/*
+		//
+		// implement with gin-gonic
+		//
+		addr := ":3000"
+		mode := os.Getenv("GIN_MODE")
+		g := gin.New()
+		g.GET("/test1", test1)
+		g.GET("/test2", test2)
 
-	if mode == "release" {
-		log.Fatal(gateway.ListenAndServe(addr, g))
-	} else {
-		log.Fatal(http.ListenAndServe(addr, g))
-	}
+		if mode == "release" {
+			log.Fatal(gateway.ListenAndServe(addr, g))
+		} else {
+			log.Fatal(http.ListenAndServe(addr, g))
+		}
+	*/
+
+	//
+	// implement with aws-sdk-lambda
+	//
+	lambda.Start(LongRunningHandler)
 }
