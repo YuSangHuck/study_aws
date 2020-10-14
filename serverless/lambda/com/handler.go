8fa2b53b7b66f6e8faf1363a699d8537d43a6ae8
YuSangHuck <g01099387636@gmail.com>
2020-10-14 19:34:09 +0900

    Update lambda handler using proxy

--- serverless/lambda/com/handler.go
+++ serverless/lambda/com/handler.go
@@ -3,41 +3,22 @@ package main
 import (
 	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	/*
-		//
-		// implement with gin-gonic
-		//
-		"log"
-		"net/http"
-		"os"
-		"github.com/apex/gateway"
-		"github.com/gin-gonic/gin"
-	*/)
+	//
+	// implement with gin-gonic
+	//
+	// "log"
+	// "net/http"
+	// "os"
+	// "github.com/apex/gateway"
+	// "github.com/gin-gonic/gin"
+)
 
-/*
-//
-// implement with gin-gonic
-//
-func test1(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"success": true, "message": "test1 lambda API"})
-}
-func test2(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"success": true, "message": "test2 lambda API"})
-}
-*/
+func HandleLambdaEvent(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println("Received body: ", request.Body)
 
-type MyEvent struct {
-	Name string `json:"What is your name?"`
-	Age  int    `json:"How old are you?"`
-}
-
-type MyResponse struct {
-	Message string `json:"Answer:"`
-}
-
-func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
-	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
 }
 
 func main() {
--- serverless/lambda/go.mod
+++ serverless/lambda/go.mod
@@ -3,8 +3,8 @@ module github.com/src/lambda
 go 1.15
 
 require (
-	github.com/apex/gateway v1.1.1 // indirect
+	github.com/apex/gateway v1.1.1
 	github.com/aws/aws-lambda-go v1.6.0
-	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/gin-gonic/gin v1.6.3
 	github.com/pkg/errors v0.9.1 // indirect
 )
