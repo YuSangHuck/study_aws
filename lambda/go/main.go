442eb2ce79275c0410171c7f236e88c11f8d1528
YuSangHuck <g01099387636@gmail.com>
2020-10-22 10:50:52 +0900

    update main

--- lambda/go/main.go
+++ lambda/go/main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 )
 
 func LongRunningHandler(ctx context.Context) (events.APIGatewayProxyResponse, error) {
+	marshaledContext, err := json.Marshal(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{Body: "", StatusCode: 500}, err
+	}
+	fmt.Println("Marshaled ctx: ", string(marshaledContext))
+
+	lc, _ := lambdacontext.FromContext(ctx)
+	marshaledLc, err := json.Marshal(lc)
+	if err != nil {
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{Body: "", StatusCode: 500}, err
+	}
+	fmt.Println("Marshaled lc: ", string(marshaledLc))
+
 	return events.APIGatewayProxyResponse{Body: "반갑수다", StatusCode: 200}, nil
 	// deadline, _ := ctx.Deadline()
 	// deadline = deadline.Add(-100 * time.Millisecond)
